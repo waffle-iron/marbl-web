14931b98d40504672de3000ebf35d8d48fbf2ce0
Brenda <hello@brendakaing.com>
2017-03-09 16:38:28 -0800

    added cookies

--- server.js
+++ server.js
@@ -9,32 +9,37 @@ var mongoose = require( 'mongoose' ),
     port     = process.env.PORT || 8000,
     app      = express();
 
-var sessionConfig = {
-     secret:'CookieMonster', // Secret name for decoding secret and such
-     resave:false, // Don't resave session if no changes were made
-     saveUninitialized: true, // Don't save session if there was nothing initialized
-     name:'myCookie', // Sets a custom cookie name
-     cookie: {
-      secure: false, // This need to be true, but only on HTTPS
-      httpOnly:false, // Forces cookies to only be used over http
-      maxAge: 3600000
- }
-}
+// var sessionConfig = {
+//      secret:'CookieMonster', // Secret name for decoding secret and such
+//      resave:false, // Don't resave session if no changes were made
+//      saveUninitialized: true, // Don't save session if there was nothing initialized
+//      name:'myCookie', // Sets a custom cookie name
+//      cookie: {
+//       secure: false, // This need to be true, but only on HTTPS
+//       httpOnly:false, // Forces cookies to only be used over http
+//       maxAge: 3600000
+//  }
+// }
+
+var cookieSession = require('cookie-session')
+app.set('trust proxy', 1)
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+}))
 
 // app.use( express.static( path.join( root, 'bower_components' )));
 app.use(bp.urlencoded({extended:true}))
 app.use(bp.json({extended: true}));
 app.use( express.static( path.join( root, 'client')));
-app.use(session(sessionConfig));
-// app.use( express.session({ store: new MongoStore({ db: 'some-database' }) }));
+app.get('/', function (req, res, next) {
+  // Update views
+  req.session.views = (req.session.views || 0) + 1
+  // Write response
+  res.end(req.session.views + ' views')
+})
+// app.use(session(sessionConfig));
 
-function sessionCleanup() {
-    sessionStore.all(function(err, sessions) {
-        for (var i = 0; i < sessions.length; i++) {
-            sessionStore.get(sessions[i], function() {} );
-        }
-    });
-}
 
 app.listen( 8000, function() {
   console.log( `server running on port 8000` );
