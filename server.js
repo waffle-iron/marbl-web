d117d8daf940f955bfb1f81001534f4baf1869b8
Brenda <hello@brendakaing.com>
2017-03-09 16:19:55 -0800

    v1

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ var sessionConfig = {
  }
 }
 
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 // app.use( express.static( path.join( root, 'bower_components' )));
 app.use(bp.urlencoded({extended:true}))
 app.use(bp.json({extended: true}));
