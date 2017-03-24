f8d0f9681c64233ea55ebe0615b64d4d9d2daff6
ishmaelthedestroyer <ishmaelthedestroyer@gmail.com>
2017-03-24 11:29:58 -0700

    restructure; lighter server; added ejs

---
+++ app.js
@@ -0,0 +1,44 @@
+/**
+ * server entry point
+ * @module app
+ */
+
+var
+
+  /** express module */
+  express = require("express"),
+
+  /** server instance */
+  app = express(),
+
+  /** the port the server will listen to for requests */
+  port = process.env.PORT || process.argv[2] || 8000;
+
+app.use(express.static(__dirname + "/public", {
+  // disable cache while developing
+  // maxAge: 2592000000 // 30 day cache
+}))
+
+// use ejs templating engine
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+/** routes */
+app.get("/welcome", function(req, res) {
+  res.render(__dirname + "/public/welcome.ejs", {
+    // ...
+  });
+});
+
+/** handler for uncaught routes */
+app.get("*", function(req, res) {
+  res.render("index");
+});
+
+/** take requests on specified port */
+app.listen(port);
+console.log("Server listening on port " + port + ".");
+
+/** expose app */
+module.exports = app;
