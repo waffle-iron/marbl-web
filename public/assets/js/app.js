1fb34b78ef955969e2eae1464cf5103136ef4b56
ishmaelthedestroyer <ishmaelthedestroyer@gmail.com>
2017-03-24 13:56:27 -0700

    submit form, redirect to welcome on success

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,49 @@
+/* ==========================================================================
+// Helper Functions
+========================================================================== */
+
+/**
+ * Generate RFC-compliant uuid
+ * @return {string}
+ */
+function uuid() {
+  var d;
+  d = Date.now();
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+    .replace(/[xy]/g, function(c) {
+      var r;
+      r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c === "x" ? r : r & 0x7 | 0x8).toString(16);
+    });
+};
+
+/**
+ * checks if a string is a valid email
+ * @param input {String} string to check
+ * @returns {Boolean} whether or not the email is valid
+ */
+function isValidEmail(input) {
+  return /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/.test(input);
+};
+
+/**
+ * checks how far the page is scrolled down, used to show & hide nav
+ */
+function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+    if ($(window).scrollTop() > startY) {
+        $('.main_h').addClass("scrolled");
+    } else {
+        $('.main_h').removeClass("scrolled");
+    }
+}
+
+/* ==========================================================================
+// Config functions
+========================================================================== */
+
 $(window).load( function(){
   $('.main_h').addClass('sticky');
 });
@@ -9,16 +55,6 @@ $(window).scroll(function() {
         $('.main_h').addClass('sticky');
     }
 });
-
-function checkScroll(){
-    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
-
-    if($(window).scrollTop() > startY){
-        $('.main_h').addClass("scrolled");
-    }else{
-        $('.main_h').removeClass("scrolled");
-    }
-}
 
 if($('.main_h').length > 0){
     $(window).on("scroll load resize", function(){
@@ -35,5 +71,59 @@ $('#form').submit(function(e) {
   e.preventDefault();
   e.stopPropagation();
 
-  console.log('we did it');
+  var
+    name = $.trim($('#name').val()),
+    email = $.trim($('#email').val()),
+    instagram = $.trim($('#instagram').val()),
+    $button = $('#submit'),
+    browser = Cookies.get('_b_');
+
+  if (!name) {
+    alert('You must enter your name.');
+    return;
+  } else if (!isValidEmail(email)) {
+    alert('You must enter a valid email.');
+    return;
+  } else if (!instagram) {
+    alert('You must enter your Instagram.');
+    return;
+  }
+
+  if (!browser) {
+    browser = uuid();
+    Cookies.set('_b_', browser);
+  }
+
+  $button.attr('disabled', true);
+  $button.attr('value', 'APPLYING ...');
+
+  $.post('http://api.marbl.io/v1/invites', {
+    browser: browser,
+    name: name,
+    email: email,
+    instagram: instagram,
+  }).success(function(data) {
+    var
+      code = data.code,
+      place = data.placeInQueue,
+      path = '/welcome?code=' + code + '&place=' + place;
+
+    window.location = path;
+  }).error(function(error) {
+    console.log('error', arguments);
+    $button.attr('disabled', false);
+    $button.attr('value', 'APPLY');
+
+    var message = 'Uh-oh. We were unable to process your application. Please try again.';
+
+    try {
+      message = JSON.parse(error.responseText).message;
+    } catch (e) {
+      console.log(e);
+    }
+
+    alert(message);
+  });
+
+  console.log('we did it', name, email, instagram);
 });
